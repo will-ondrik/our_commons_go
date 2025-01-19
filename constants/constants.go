a0f4ef9d878c46ad4e928b056cad1e5da477d388
Will Ondrik <wondrik@my.bcit.ca>
2025-01-18 18:44:34 -0800

    Constants init.

---
+++ constants/constants.go
@@ -0,0 +1,15 @@
+package constants
+
+var (
+
+	// Expense categories
+	TRAVEL      = "Travel"
+	HOSPITALITY = "Hospitality"
+	CONTRACT    = "Contract"
+
+	// Base MP expenditures URL
+	BASE_URL = "https://www.ourcommons.ca"
+
+	// Naming
+	VACANT = "Vacant"
+)
