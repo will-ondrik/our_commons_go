5f974fcd8d241e583650bf36a4c52985e05e21fe
Will Ondrik <wondrik@my.bcit.ca>
2025-01-21 07:53:05 -0800

    Modified main to be more modular, allowing different files to handle their own repespective responsibilites of the program.

--- main.go
+++ main.go
@@ -1,65 +1,47 @@
 package main
 
 import (
-	"context"
+	"etl_our_commons/browser"
 	"etl_our_commons/dtos"
-	"etl_our_commons/extract"
 	"fmt"
 	"log"
-	"strings"
-	"time"
-
-	"github.com/PuerkitoBio/goquery"
-	"github.com/chromedp/chromedp"
 )
 
 func main() {
-	// Set Chrome options
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", true),
-		chromedp.Flag("disable-gpu", false),
-	)
+	browser := &browser.Browser{}
 
-	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
-	defer cancel()
-
-	ctx, cancel := chromedp.NewContext(allocCtx)
-	defer cancel()
-
-	ctx, cancel = context.WithTimeout(ctx, 10*time.Second)
-	defer cancel()
-
-	url := "https://www.ourcommons.ca/proactivedisclosure/en/members"
-	var tableHTML string
-
-	err := chromedp.Run(ctx,
-		chromedp.Navigate(url),
-		chromedp.WaitVisible("table.table"),
-		chromedp.OuterHTML("table.table", &tableHTML),
-	)
-	if err != nil {
-		fmt.Println("Error during Chromedp:", err)
-		return
+	task := dtos.Task{
+		Type:               "extractMps",
+		Url:                "https://www.ourcommons.ca/proactivedisclosure/en/members/2022/1",
+		ExtractFromElement: "table.table",
 	}
 
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(tableHTML))
-
-	// extract MP Expense Categories
-	var mps []*dtos.MpWithExpenseCategories
-	mps, err = extract.Mps(doc)
+	mpData, err := browser.RunTask(task)
 	if err != nil {
-		log.Printf("Error extracting MP data: %v", err)
+		log.Fatal(err)
 	}
 
-	// divides mp's into chunks for multithreaded processing
-
-	fmt.Println(mps)
-
-	err = chromedp.Cancel(ctx)
-	if err != nil {
-		fmt.Println("Failed to close chrome instance")
-		return
+	fmt.Println(mpData.([]*dtos.MpWithExpenseCategories))
+	for _, mp := range mpData.([]*dtos.MpWithExpenseCategories) {
+		fmt.Printf("\nMP: %+v\n", mp)
 	}
-	fmt.Println("Script completed")
+
+	for i, mp := range mpData.([]*dtos.MpWithExpenseCategories) {
+		if i == 0 {
+			task = dtos.Task{
+				Type:               "extractTravelExpenses",
+				Url:                mp.TravelExpenses.Href,
+				ExtractFromElement: "#data-table",
+			}
+
+			mpTravelExpenses, err := browser.RunTask(task)
+			if err != nil {
+				fmt.Printf("\nTravel expenses extraction failed: %v", err)
+			}
+
+			fmt.Println(mpTravelExpenses.([]*dtos.TravelExpenses))
+		}
+
+	}
 
 }
