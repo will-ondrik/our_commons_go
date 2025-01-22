395a868b818b21a8c9375740c95b9a9248d2105d
Will Ondrik <wondrik@my.bcit.ca>
2025-01-21 19:45:46 -0800

    MpTravelExpenses now extracts all necessary data.

--- extract/extract.go
+++ extract/extract.go
@@ -110,171 +110,123 @@ func MpTravelExpenses(doc *goquery.Document) ([]*dtos.TravelExpenses, error) {
 	for i := 0; i < rows.Length(); i++ {
 		row := rows.Eq(i)
 
-		if rows.HasClass("expenses-main-info") {
+		// Process the visible row
+		if row.HasClass("expenses-main-info") {
+			var travelExpenses dtos.TravelExpenses
+
 			row.Find("td").Each(func(j int, cell *goquery.Selection) {
 				text := strings.TrimSpace(cell.Text())
-				fmt.Println("normal content: ", text)
-			})
-
-		}
-
-		// Process hidden row
-		if i+1 < rows.Length() {
-			hiddenRow := rows.Eq(i + 1)
-
-			if hiddenRow.Find("table").Length() > 0 {
-				hiddenRow.Find("table tbody tr").Each(func(l int, hiddenRow *goquery.Selection) {
-					hiddenRow.Find("td").Each(func(m int, cell *goquery.Selection) {
-						text := strings.TrimSpace(cell.Text())
-						fmt.Println("hidden content:", text)
-
-					})
-				})
-				i++
-			}
-
-		}
-
-	}
-	/*
-		doc.Find("tbody tr").Each(func(i int, row *goquery.Selection) {
-			if parseErr != nil {
-				return
-			}
-
-			travelExpenses := dtos.TravelExpenses{}
-
-			// Visible row
-			if i%2 == 0 {
-				row.Find("td").Each(func(j int, cell *goquery.Selection) {
-					text := strings.TrimSpace(cell.Text())
-					switch j {
-					case 0:
-						travelExpenses.Claim = text
-					case 1:
-						travelExpenses.Dates = format.StringToDateRange(text)
-					case 2:
-						transportationCosts, err := format.ExpenseToFloat(text)
-						if err != nil {
-							parseErr = err
-							return
-						}
-						travelExpenses.TravelCosts.Transportation = transportationCosts
-					case 3:
-						accomodationCosts, err := format.ExpenseToFloat(text)
-						if err != nil {
-							parseErr = err
-							return
-						}
-						travelExpenses.TravelCosts.Accomodation = accomodationCosts
-					case 4:
-						mealsAndIncidentalsCost, err := format.ExpenseToFloat(text)
-						if err != nil {
-							parseErr = err
-							return
-						}
-						travelExpenses.TravelCosts.MealsAndIncidentals = mealsAndIncidentalsCost
-					case 5:
-						points, err := format.FlightPointsToFloat(text)
-						if err != nil {
-							parseErr = err
-							return
-						}
-						travelExpenses.FlightPoints.Regular = points
-					case 6:
-						points, err := format.FlightPointsToFloat(text)
-						if err != nil {
-							parseErr = err
-							return
-						}
-						travelExpenses.FlightPoints.Special = points
-					case 7:
-						points, err := format.FlightPointsToFloat(text)
-						if err != nil {
-							parseErr = err
-							return
-						}
-						travelExpenses.FlightPoints.USA = points
-					case 8:
-						costs, err := format.ExpenseToFloat(text)
-						if err != nil {
-							parseErr = err
-							return
-						}
-						travelExpenses.TravelCosts.Total = costs
+				switch j {
+				case 0:
+					travelExpenses.Claim = text
+				case 1:
+					travelExpenses.Dates = format.StringToDateRange(text)
+				case 2:
+					transportationCosts, err := format.ExpenseToFloat(text)
+					if err != nil {
+						parseErr = err
+						return
 					}
-				})
-			}
-
-			fmt.Println("Moving to hidden table..")
-			// Handle hidden row
-			hiddenRow := row.Next()
-			if hiddenRow.Length() == 0 {
-				fmt.Println("No hidden row found")
-				return
-			}
-
-			hiddenRowTable := hiddenRow.Find("table")
-			if hiddenRowTable.Length() == 0 {
-				fmt.Println("No table found in hidden row")
-				return
-			}
-			hiddenRow.Find("table tbody tr").Each(func(k int, row *goquery.Selection) {
-				if parseErr != nil {
-					fmt.Println("error finding hidden table")
-					return
+					travelExpenses.TravelCosts.Transportation = transportationCosts
+				case 3:
+					accommodationCosts, err := format.ExpenseToFloat(text)
+					if err != nil {
+						parseErr = err
+						return
+					}
+					travelExpenses.TravelCosts.Accomodation = accommodationCosts
+				case 4:
+					mealsAndIncidentals, err := format.ExpenseToFloat(text)
+					if err != nil {
+						parseErr = err
+						return
+					}
+					travelExpenses.TravelCosts.MealsAndIncidentals = mealsAndIncidentals
+				case 5:
+					points, err := format.FlightPointsToFloat(text)
+					if err != nil {
+						parseErr = err
+						return
+					}
+					travelExpenses.FlightPoints.Regular = points
+				case 6:
+					points, err := format.FlightPointsToFloat(text)
+					if err != nil {
+						parseErr = err
+						return
+					}
+					travelExpenses.FlightPoints.Special = points
+				case 7:
+					points, err := format.FlightPointsToFloat(text)
+					if err != nil {
+						parseErr = err
+						return
+					}
+					travelExpenses.FlightPoints.USA = points
+				case 8:
+					total, err := format.ExpenseToFloat(text)
+					if err != nil {
+						parseErr = err
+						return
+					}
+					travelExpenses.TravelCosts.Total = total
 				}
-
-				var traveller dtos.Traveller
-
-				row.Find("td").Each(func(l int, cell *goquery.Selection) {
-					numCols := row.Find("td").Length()
-					fmt.Println("Hidden row", l)
-					text := strings.TrimSpace(cell.Text())
-
-					// Handle case where transportation doesn't include flights
-					// This case only provides Traveller Name, Traveller Type,
-					// Purpose of Travel and City
-					if numCols == 4 {
-						switch l {
-						case 0:
-							traveller.Name = format.TravellerName(text)
-						case 1:
-							traveller.Type = text
-						case 2:
-							traveller.Purpose = text
-						case 3:
-							formattedCity := format.City(text)
-							traveller.DepartureCity = formattedCity
-							traveller.DestinationCity = formattedCity
-						}
-						traveller.Date = "Not Provided"
-						// Handle normal case
-					} else {
-						switch l {
-						case 0:
-							traveller.Name = format.TravellerName(text)
-						case 1:
-							traveller.Type = text
-						case 2:
-							traveller.Purpose = text
-						case 3:
-							traveller.Date = text
-						case 4:
-							traveller.DepartureCity = text
-						case 5:
-							traveller.DepartureCity = text
-						}
-					}
-				})
-				travelExpenses.TravelLogs = append(travelExpenses.TravelLogs, traveller)
 			})
-			if parseErr == nil {
-				mpTravelExpenses = append(mpTravelExpenses, &travelExpenses)
-			}
-		})*/
-	return mpTravelExpenses, parseErr
 
+			// Check for the hidden row
+			if i+1 < rows.Length() {
+				hiddenRow := rows.Eq(i + 1)
+				if hiddenRow.Find("table").Length() > 0 {
+					hiddenRow.Find("table tbody tr").Each(func(k int, nestedRow *goquery.Selection) {
+						var traveller dtos.Traveller
+						numCols := nestedRow.Find("td").Length()
+
+						nestedRow.Find("td").Each(func(l int, cell *goquery.Selection) {
+							text := strings.TrimSpace(cell.Text())
+							// Case for transportation without flights
+							if numCols == 4 {
+								switch l {
+								case 0:
+									traveller.Name = format.TravellerName(text)
+								case 1:
+									traveller.Type = text
+								case 2:
+									traveller.Purpose = text
+								case 3:
+									formattedCity := format.City(text)
+									traveller.DepartureCity = formattedCity
+									traveller.DestinationCity = formattedCity
+								}
+								traveller.Date = "Not Provided"
+							} else { // Normal case
+								switch l {
+								case 0:
+									traveller.Name = format.TravellerName(text)
+								case 1:
+									traveller.Type = text
+								case 2:
+									traveller.Purpose = text
+								case 3:
+									traveller.Date = text
+								case 4:
+									traveller.DepartureCity = text
+								case 5:
+									traveller.DestinationCity = text
+								}
+							}
+						})
+
+						travelExpenses.TravelLogs = append(travelExpenses.TravelLogs, traveller)
+					})
+					i++
+				}
+			}
+
+			mpTravelExpenses = append(mpTravelExpenses, &travelExpenses)
+		}
+	}
+
+	return mpTravelExpenses, parseErr
 }
 
 func MpHospitalityExpenses() {
