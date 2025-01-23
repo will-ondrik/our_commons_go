5d7ac14b5d0f6e7457ca4654d378de7afe65b24f
Will Ondrik <wondrik@my.bcit.ca>
2025-01-22 18:28:10 -0800

    Hospitality expense-related structs.
    HospitalityExpense holds all information related to the expense entry.
    
    Event contains the event type and the individual expenses associated with that event.
    
    ExpenseLogs contains the claim number, the supplier and the cost of that expense.

--- dtos/hospitality_expenses.go
+++ dtos/hospitality_expenses.go
@@ -1,1 +1,19 @@
 package dtos
+
+type HospitalityExpense struct {
+	Date       string
+	Location   string
+	Attendance int
+	Purpose    string
+	Event      Event
+	TotalCost  float64
+}
+type Event struct {
+	Type        string
+	ExpenseLogs []ExpenseLogs
+}
+type ExpenseLogs struct {
+	Claim    string
+	Supplier string
+	Cost     float64
+}
