500683fd6632052d620a8500a6f4c03f5ea819c9
Will Ondrik <wondrik@my.bcit.ca>
2025-01-26 19:31:00 -0800

    Moved hospitality extractoin to its own file.

---
+++ extract/hospitality_expenses.go
@@ -0,0 +1,86 @@
+package extract
+
+import (
+	"etl_our_commons/dtos"
+	format "etl_our_commons/formatting"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func MpHospitalityExpenses(doc *goquery.Document) ([]*dtos.HospitalityExpense, error) {
+	var hospitalityExpenses []*dtos.HospitalityExpense
+	var parseErr error
+
+	rows := doc.Find("tbody tr")
+	for i := 0; i < rows.Length(); i++ {
+		row := rows.Eq(i)
+
+		if !row.HasClass("expenses-main-info") {
+			continue
+		}
+
+		hospitalityExpense := &dtos.HospitalityExpense{}
+		row.Find("td").Each(func(j int, cell *goquery.Selection) {
+			text := strings.TrimSpace(cell.Text())
+			switch j {
+			case 0:
+				hospitalityExpense.Date = text
+			case 1:
+				hospitalityExpense.Location = text
+			case 2:
+				attendance, err := strconv.Atoi(text)
+				if err != nil {
+					parseErr = err
+					return
+				}
+				hospitalityExpense.Attendance = attendance
+			case 3:
+				hospitalityExpense.Purpose = text
+			case 4:
+				cost, err := format.ExpenseToFloat(text)
+				if err != nil {
+					parseErr = err
+					return
+				}
+				hospitalityExpense.TotalCost = cost
+			}
+		})
+
+		// Process hidden data
+		hiddenRow := rows.Eq(i + 1)
+
+		// Retrieve Event type
+		eventType := hiddenRow.Find(".col-md-4").Text()
+		hospitalityExpense.Event.Type = format.EventType(eventType)
+
+		var expenseLogs []dtos.ExpenseLogs
+		hiddenRow.Find("table tbody tr").Each(func(k int, nestedRow *goquery.Selection) {
+			var expenseEntry dtos.ExpenseLogs
+			nestedRow.Find("td").Each(func(l int, cell *goquery.Selection) {
+				text := strings.TrimSpace(cell.Text())
+				switch l {
+				case 0:
+					expenseEntry.Claim = text
+				case 1:
+					expenseEntry.Supplier = text
+				case 2:
+					expense, err := format.ExpenseToFloat(text)
+					if err != nil {
+						parseErr = err
+						return
+					}
+					expenseEntry.Cost = expense
+				}
+			})
+			if expenseEntry.Claim == "" || expenseEntry.Cost == 0 || expenseEntry.Supplier == "" {
+				return
+			}
+			expenseLogs = append(expenseLogs, expenseEntry)
+		})
+		hospitalityExpense.Event.ExpenseLogs = expenseLogs
+		hospitalityExpenses = append(hospitalityExpenses, hospitalityExpense)
+	}
+	return hospitalityExpenses, parseErr
+}
