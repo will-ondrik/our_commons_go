8a0e1f48f0cd6decc5210cce69cf04bdf2722407
Will Ondrik <wondrik@my.bcit.ca>
2025-01-27 07:08:23 -0800

    Polling extracts all reports from the site, any report that hasn't been stored in the database is a report that needs to have expenditure data extracted.

---
+++ extract/polling.go
@@ -0,0 +1,56 @@
+package extract
+
+import (
+	"etl_our_commons/constants"
+	"etl_our_commons/dtos"
+	"fmt"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func ExpenditureReports(doc *goquery.Document) dtos.AllExpenditureReports {
+	var expenditureReports dtos.AllExpenditureReports
+
+	ul := doc.Find("ul")
+	if !ul.HasClass("nav navbar-nav") && ul.Has("ul#ce-hoc-nav-parliamentary-business").Length() == 0 {
+		ul.Find("li").Each(func(i int, cell *goquery.Selection) {
+			var expenditureReport dtos.ExpenditureReport
+			if i > 6 {
+				text := strings.TrimSpace(cell.Text())
+				if len(text) > 9 {
+					quarter, err := FiscalQuarter(text)
+					if err != nil {
+						fmt.Println(err)
+					}
+					dateRange := ReportDates(text)
+					yearRange := ReportYears(dateRange)
+
+					cell.Find("a").Each(func(j int, link *goquery.Selection) {
+						href, exists := link.Attr("href")
+						if !exists {
+							fmt.Println("No href")
+						}
+						hrefLink := fmt.Sprintf("%s%s", constants.BASE_URL, href)
+
+						expenditureReport = dtos.ExpenditureReport{
+							Years:     yearRange,
+							DateRange: dateRange,
+							Quarter:   quarter,
+							Href:      hrefLink,
+						}
+					})
+
+					expenditureReports.Reports = append(expenditureReports.Reports, expenditureReport)
+				}
+
+			}
+		})
+		for _, report := range expenditureReports.Reports {
+			fmt.Printf("Report: %+v\n\n", report)
+		}
+
+	}
+
+	return expenditureReports
+}
