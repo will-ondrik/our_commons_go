2209a3179e6646189c4d7e444bb991771cba1d9d
Will Ondrik <wondrik@my.bcit.ca>
2025-02-05 07:57:54 -0800

    Removed tasks. They're now located in the tasks pkg.

--- constants/constants.go
+++ constants/constants.go
@@ -1,13 +1,10 @@
 package constants
 
 import (
-	"etl_our_commons/dtos"
-
 	"github.com/chromedp/chromedp"
 )
 
 var (
-
 	// Expense categories
 	TRAVEL      = "Travel"
 	HOSPITALITY = "Hospitality"
@@ -20,37 +17,7 @@ var (
 	VACANT = "Vacant"
 
 	CHROME_OPTIONS = append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", false),
+		chromedp.Flag("headless", true),
 		chromedp.Flag("disable-gpu", false),
 	)
-
-	MpTask = dtos.Task{
-		Type:               "extractMps",
-		Url:                "",
-		ExtractFromElement: "body",
-	}
-
-	TravelTask = dtos.Task{
-		Type:               "extractTravelExpenses",
-		Url:                "",
-		ExtractFromElement: "body",
-	}
-
-	HospitalityTask = dtos.Task{
-		Type:               "extractHospitalityExpenses",
-		Url:                "",
-		ExtractFromElement: "body",
-	}
-
-	ContractTask = dtos.Task{
-		Type:               "extractContractExpenses",
-		Url:                "",
-		ExtractFromElement: "body",
-	}
-
-	PollingTask = dtos.Task{
-		Type:               "polling",
-		Url:                "https://www.ourcommons.ca/proactivedisclosure/en/members/2022/1",
-		ExtractFromElement: "main.ce-hoc-body-content",
-	}
 )
