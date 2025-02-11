8fffaf122c7f63dc7281dc52a15205c3ec957fcb
Will Ondrik <wondrik@my.bcit.ca>
2025-02-11 14:34:02 -0800

    Incorporated handling of failed tasks and processing of scraped MP html.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"etl_our_commons/browser"
 	"etl_our_commons/dtos"
+	"etl_our_commons/processing"
 	"etl_our_commons/tasks"
 	"fmt"
 	"sync"
@@ -10,95 +11,18 @@ import (
 )
 
 /*
-func main() {
-	startTime := time.Now()
-	browser := &browser.Browser{}
-
-	task := constants.PollingTask
-	reports, err := browser.RunTask(task)
-	if err != nil {
-		fmt.Println("Err")
-	}
-
-	expenditureReports := reports.(dtos.AllExpenditureReports)
-	for i, report := range expenditureReports.Reports {
-		if i == 1 {
-			break
-		}
-		mpTask := constants.MpTask
-		mpTask.Url = report.Href
-
-		mps, err := browser.RunTask(mpTask)
-		if err != nil {
-			fmt.Println("Error extract MP data: ", err)
-		}
-
-		mpData := mps.([]*dtos.MpWithExpenseCategories)
-		for _, mp := range mpData {
-
-				var travelExpenses []*dtos.TravelExpense
-				var contractExpenses []*dtos.ContractExpense
-				var hospitalityExpenses []*dtos.HospitalityExpense
-
-				if mp.TravelExpenses.Href != "" {
-					tt := constants.TravelTask
-					tt.Url = mp.TravelExpenses.Href
-
-					travelData, err := browser.RunTask(tt)
-					if err != nil {
-						fmt.Println("Travel extraction error: ", err)
-					}
-
-					travelExpenses = travelData.([]*dtos.TravelExpense)
-
-				}
-
-				if mp.ContractExpenses.Href != "" {
-					ct := constants.ContractTask
-					ct.Url = mp.ContractExpenses.Href
-
-					contractData, err := browser.RunTask(ct)
-					if err != nil {
-						fmt.Println("Contract extraction error: ", err)
-					}
-					contractExpenses = contractData.([]*dtos.ContractExpense)
-				}
-
-				if mp.HospitalityExpenses.Href != "" {
-					ht := constants.HospitalityTask
-					ht.Url = mp.HospitalityExpenses.Href
-
-					hospitalityData, err := browser.RunTask(ht)
-					if err != nil {
-						fmt.Println("Hospitality extraction error: ", err)
-					}
-
-					hospitalityExpenses = hospitalityData.([]*dtos.HospitalityExpense)
-				}
-
-				fmt.Println("Travel expenses: ", travelExpenses)
-				fmt.Println("Contract expenses: ", contractExpenses)
-				fmt.Println("Hospitality expenses: ", hospitalityExpenses)
-		}
-	}
-
-	runTime := time.Now().Sub(startTime)
-	fmt.Println("Total Runtime: ", runTime)
-}
-*/
-
-/*
 Runtimes
-- Sequential runtime (commented out): 47 minutes
-- Concurrent runtime (updated code): 27 minutes, 36 seconds
+- Sequential runtime (commented out): 47 minutes per report
+- Concurrent runtime (updated code): 24 minutes, 36 seconds per report
 */
 
 // Worker and Rate Limits
-const WorkerLimit = 2
+const WorkerLimit = 4
 const RequestsPerSecond = 2
 
 func main() {
 	startTime := time.Now()
+	defer getRuntime(startTime)
 	b := &browser.Browser{}
 
 	tm := tasks.NewTaskManager(b)
@@ -107,6 +31,10 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	var htmlStore []*dtos.MpHtml
+	var redoTaskQueue []*dtos.RedoTask
+	var mu sync.Mutex
 
 	// Create channel and rate limiter
 	// Rate limited ensures set time between requests
@@ -118,11 +46,13 @@ func main() {
 	// TODO: Test runtime with larger pool
 	for i := 0; i < WorkerLimit; i++ {
 		wg.Add(1)
-		go tm.ProcessMpQueue(mpQueue, &wg, rateLimiter)
+		//go tm.ProcessMpQueue(mpQueue, &wg, rateLimiter)
+		go tm.ProcessMpQueue2(mpQueue, &wg, rateLimiter, &htmlStore, &mu, &redoTaskQueue)
 	}
 
 	// Extract and add MPs to the processing queue
 	for i, report := range expenditures.Reports {
+
 		if i == 1 {
 			break
 		}
@@ -132,6 +62,8 @@ func main() {
 		}
 
 		for _, mp := range mps {
+			mp.Year = report.Years
+			mp.Quarter = report.Quarter
 			// Send MPs to pool
 			mpQueue <- mp
 		}
@@ -142,6 +74,23 @@ func main() {
 	close(mpQueue)
 	wg.Wait()
 
-	runTime := time.Since(startTime)
-	fmt.Println("Total Runtime:", runTime)
+	// Reprocess failed tasks
+	fmt.Println("\nHandling failed tasks...")
+	tm.ProcessRedoTasks(redoTaskQueue, &htmlStore)
+
+	for _, html := range htmlStore {
+		fmt.Println(html)
+	}
+	fmt.Println("\nProcessing html store...")
+	mps := processing.ProcessData(htmlStore)
+	for _, mp := range mps {
+		fmt.Println("-----------------------------------")
+		fmt.Printf("MP Info: %+v\n", mp)
+		fmt.Println("-----------------------------------")
+	}
+
+}
+
+func getRuntime(elapsed time.Time) {
+	fmt.Println("Total Runtime: ", time.Since(elapsed))
 }
