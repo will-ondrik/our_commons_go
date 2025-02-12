3e5e2209df691403ea5e207cbd75dcdd3d72f324
Will Ondrik <wondrik@my.bcit.ca>
2025-02-11 17:14:24 -0800

    Mp struct contains all relevant MP information from scraping tasks.

--- dtos/mp.go
+++ dtos/mp.go
@@ -2,15 +2,15 @@ package dtos
 
 // Expendtiture page dtos
 type MpWithExpenseCategories struct {
-	MpName              Name     `json:"mpName"`
-	Constituency        string   `json:"constituency"`
-	Caucus              string   `json:"caucus"`
-	Salaries            float64  `json:"salaries"`
-	TravelExpenses      Category `json:"travelExpenses"`
-	HospitalityExpenses Category `json:"hospitalityExpenses"`
-	ContractExpenses    Category `json:"contractExpenses"`
-	Year                string   `json:"year"`
-	Quarter             int      `json:"quarter"`
+	MpName              Name      `json:"mpName"`
+	Constituency        string    `json:"constituency"`
+	Caucus              string    `json:"caucus"`
+	Salaries            float64   `json:"salaries"`
+	TravelExpenses      Category  `json:"travelExpenses"`
+	HospitalityExpenses Category  `json:"hospitalityExpenses"`
+	ContractExpenses    Category  `json:"contractExpenses"`
+	Year                DateRange `json:"year"`
+	Quarter             int       `json:"quarter"`
 }
 
 type Category struct {
@@ -23,4 +23,28 @@ type MpExpensesResults struct {
 	HospitalityExpenses []*HospitalityExpense
 	ContractExpenses    []*ContractExpense
 	Errors              []error
+}
+
+type Mp struct {
+	MpName        Name
+	Constituency  string
+	Caucus        string
+	Year          string
+	FiscalQuarter int
+	Expenses      Expenses
+}
+
+type Expenses struct {
+	Totals              ExpenseTotals
+	ContractExpenses    []*ContractExpense
+	HospitalityExpenses []*HospitalityExpense
+	TravelExpenses      []*TravelExpense
+}
+
+type ExpenseTotals struct {
+	SalariesCost    float64
+	ContractCost    float64
+	HospitalityCost float64
+	TravelCost      float64
+	TotalCost       float64
 }
