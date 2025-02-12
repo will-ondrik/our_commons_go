479e34e88b7be8be584fc67e464e647214f57e8b
Will Ondrik <wondrik@my.bcit.ca>
2025-02-11 17:06:40 -0800

    ProcessMpHtml concurrently scrapes contract, hospitality and travel expenses for a given MP.
    CreateTasks generates tasks for each scraping task.
    
    ProcessMpQueue2 takes URLs from a queued MP and runs the scraping tasks.
    
    AddRedoTasks queues failed tasks to be re-run after the
    
    RedoTask handles scraping tasks that failed in their first attempts.

--- tasks/task_manager.go
+++ tasks/task_manager.go
@@ -4,6 +4,7 @@ import (
 	"etl_our_commons/browser"
 	"etl_our_commons/dtos"
 	"fmt"
+	"log"
 	"sync"
 	"time"
 )
@@ -159,4 +160,184 @@ func (tm *TaskManager) ProcessMpQueue(mpQueue chan *dtos.MpWithExpenseCategories
 		tm.ProcessMp(mp)
 	}
 	wg.Done()
+}
+
+func (tm *TaskManager) ProcessMpHtml(tasks dtos.Tasks) (*dtos.MpHtml, error, dtos.TaskErrors) {
+	log.Printf("Processing MP html...\n")
+	mpHtml := &dtos.MpHtml{}
+	var wg sync.WaitGroup
+
+	var taskErrors dtos.TaskErrors
+	errChan := make(chan error, 3)
+
+	if tasks.Contract.Url == "" {
+		mpHtml.Contract = nil
+	} else {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			doc, err := tm.Browser.RunTask2(tasks.Contract)
+			if err != nil {
+				fmt.Println("[CONTRACT ERROR]", tasks.Contract.Url)
+				taskErrors.Contract = true
+				errChan <- err
+			} else {
+				mpHtml.Contract = doc
+			}
+		}()
+	}
+
+	if tasks.Hospitality.Url == "" {
+		mpHtml.Hospitality = nil
+	} else {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			time.Sleep(200 * time.Millisecond)
+			doc, err := tm.Browser.RunTask2(tasks.Hospitality)
+			if err != nil {
+				fmt.Println("[HOSPITALITY ERROR]", tasks.Hospitality.Url)
+				taskErrors.Hospitality = true
+				errChan <- err
+			} else {
+				mpHtml.Hospitality = doc
+
+			}
+		}()
+	}
+
+	if tasks.Travel.Url == "" {
+		mpHtml.Travel = nil
+	} else {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			time.Sleep(200 * time.Millisecond)
+			doc, err := tm.Browser.RunTask2(tasks.Travel)
+			if err != nil {
+				fmt.Println("[TRAVEL ERROR]", tasks.Hospitality.Url)
+				taskErrors.Travel = true
+				errChan <- err
+			} else {
+				mpHtml.Travel = doc
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	// Collect and close error channel
+	close(errChan)
+	var err error
+	for e := range errChan {
+		err = e
+	}
+
+	return mpHtml, err, taskErrors
+}
+
+func (tm *TaskManager) CreateTasks(mp *dtos.MpWithExpenseCategories) dtos.Tasks {
+	ct := ContractTask
+	ct.Url = mp.ContractExpenses.Href
+
+	ht := HospitalityTask
+	ht.Url = mp.HospitalityExpenses.Href
+
+	tt := TravelTask
+	tt.Url = mp.TravelExpenses.Href
+
+	return dtos.Tasks{
+		Contract:    ct,
+		Hospitality: ht,
+		Travel:      tt,
+	}
+}
+
+func (tm *TaskManager) ProcessMpQueue2(mpQueue chan *dtos.MpWithExpenseCategories, wg *sync.WaitGroup, rateLimiter <-chan time.Time, htmlStore *[]*dtos.MpHtml, mu *sync.Mutex, redoTaskQueue *[]*dtos.RedoTask) {
+	defer wg.Done()
+	for mp := range mpQueue {
+		<-rateLimiter
+		log.Printf("Process MPQ2 time for html")
+		tasks := tm.CreateTasks(mp)
+
+		mpHtml, err, taskErrs := tm.ProcessMpHtml(tasks)
+		mpHtml.Info = *mp
+
+		if err != nil {
+			log.Printf("[ERROR] Failed to scrape MP HTML")
+			// TODO: Add failed MPs to a slice to revisit
+			tm.AddRedoTask(redoTaskQueue, *mpHtml, tasks, taskErrs)
+
+			fmt.Printf("\n[TASK QUEUE] Added task to revisit: %v\n", tasks)
+			continue
+		}
+		log.Printf("[SUCCESS] Item queued")
+		mu.Lock()
+		*htmlStore = append(*htmlStore, mpHtml)
+		mu.Unlock()
+	}
+}
+
+func (tm *TaskManager) AddRedoTask(redoQueue *[]*dtos.RedoTask, mpHtml dtos.MpHtml, tasks dtos.Tasks, taskErrs dtos.TaskErrors) {
+	*redoQueue = append(*redoQueue, &dtos.RedoTask{
+		MpHtml:     mpHtml,
+		Tasks:      tasks,
+		TaskErrors: taskErrs,
+	})
+}
+
+func (tm *TaskManager) RedoTask(task *dtos.RedoTask) (dtos.MpHtml, error) {
+	var wg sync.WaitGroup
+
+	if task.TaskErrors.Contract {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			doc, err := tm.Browser.RunTask2(task.Tasks.Contract)
+			if err != nil {
+				fmt.Println("[REDO CONTRACT ERROR]")
+			} else {
+				task.MpHtml.Contract = doc
+			}
+		}()
+	}
+
+	if task.TaskErrors.Hospitality {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			doc, err := tm.Browser.RunTask2(task.Tasks.Hospitality)
+			if err != nil {
+				fmt.Println("[REDO HOSPITALITY ERROR]")
+			} else {
+				task.MpHtml.Hospitality = doc
+			}
+		}()
+	}
+
+	if task.TaskErrors.Travel {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			doc, err := tm.Browser.RunTask2(task.Tasks.Travel)
+			if err != nil {
+				fmt.Println("[REDO TRAVEL ERROR]")
+			} else {
+				task.MpHtml.Travel = doc
+			}
+		}()
+	}
+	wg.Wait()
+
+	return task.MpHtml, nil
+}
+
+func (tm *TaskManager) ProcessRedoTasks(redoTasks []*dtos.RedoTask, htmlStore *[]*dtos.MpHtml) {
+	for _, task := range redoTasks {
+		mpHtml, err := tm.RedoTask(task)
+		if err != nil {
+			fmt.Println(err)
+		}
+		*htmlStore = append(*htmlStore, &mpHtml)
+	}
 }
