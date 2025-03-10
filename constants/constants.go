ab3a5f57c29bac29d6dc2cd0765c7a6d60dfb3a7
Will Ondrik <wondrik@my.bcit.ca>
2025-03-10 07:37:53 -0700

    Updated constants.

--- constants/constants.go
+++ constants/constants.go
@@ -5,19 +5,43 @@ import (
 )
 
 var (
-	// Expense categories
-	TRAVEL      = "Travel"
-	HOSPITALITY = "Hospitality"
-	CONTRACT    = "Contract"
-
-	// Base MP expenditures URL
-	BASE_URL = "https://www.ourcommons.ca"
-
-	// Naming
-	VACANT = "Vacant"
 
 	CHROME_OPTIONS = append(chromedp.DefaultExecAllocatorOptions[:],
 		chromedp.Flag("headless", true),
 		chromedp.Flag("disable-gpu", false),
 	)
+
+	// Scraping attempt limits
+	ATTEMPTS = 0
+	MAX_ATTEMPTS = 5
+
+	// Urls for MP contact information
+	PARLIAMENT_MEMBERS_INFO = map[string]string{
+		"PARLIAMENT_43": "https://www.ourcommons.ca/Members/en/search?parliament=43&caucusId=all&province=all&gender=all",
+		"PARLIAMENT_44": "https://www.ourcommons.ca/Members/en/search?parliament=44&caucusId=all&province=all&gender=all",
+	}
+)
+
+const (
+	// Expense categories
+	TRAVEL      = "Travel"
+	HOSPITALITY = "Hospitality"
+	CONTRACT    = "Contract"
+
+	// Base URL
+	BASE_URL = "https://www.ourcommons.ca"
+
+	// Naming
+	VACANT = "Vacant"
+
+	// HTML scraping keywords
+	HTML_BODY = "body"
+	EMPTY_STR = ""
+
+	// Worker and rate limits
+	// for Chrome scraping
+	WORKER_LIMIT = 4
+	REQUESTS_PER_SECOND = 2
+
+
 )
