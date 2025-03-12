173c734f44f77336fa3b0b970fdd62f77ddc3556
Will Ondrik <wondrik@my.bcit.ca>
2025-03-11 18:05:42 -0700

    Removed logs.

--- formatting/format.go
+++ formatting/format.go
@@ -80,8 +80,6 @@ func FlightPointsToFloat(flightPoints string) (float64, error) {
 
 func TravellerName(name string) dtos.Name {
 	name = strings.TrimSpace(name)
-
-	// TODO: Filter out "Right Hon." and "Hon."
 	name = strings.ReplaceAll(name, "Right Hon. ", "")
 	name = strings.ReplaceAll(name, "Hon. ", "")
 
