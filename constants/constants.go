83cf26f6c9a0fe98b5fd7a0480514866be264197
Will Ondrik <wondrik@my.bcit.ca>
2025-03-18 19:34:36 -0700

    Updated constants.

--- constants/constants.go
+++ constants/constants.go
@@ -20,6 +20,18 @@ var (
 		"PARLIAMENT_43": "https://www.ourcommons.ca/Members/en/search?parliament=43&caucusId=all&province=all&gender=all",
 		"PARLIAMENT_44": "https://www.ourcommons.ca/Members/en/search?parliament=44&caucusId=all&province=all&gender=all",
 	}
+
+	// Flight keywords
+	FLIGHT_KEYWORDS = []string{
+		"travel",
+		"unite the family",
+		"to attend a national caucus meeting",
+		"to attend a regional or provincial caucus meeting",
+		"attending event with member",
+		"travel to/from constituency and Ottawa",
+		
+
+	}
 )
 
 const (
@@ -27,6 +39,11 @@ const (
 	TRAVEL      = "Travel"
 	HOSPITALITY = "Hospitality"
 	CONTRACT    = "Contract"
+
+
+	// Transporation modes
+	GROUND_TRANSPORTATION   = "car"
+	AIR_TRANSPORTATION = "plane"
 
 	// Base URL
 	BASE_URL = "https://www.ourcommons.ca"
@@ -42,6 +59,4 @@ const (
 	// for Chrome scraping
 	WORKER_LIMIT = 4
 	REQUESTS_PER_SECOND = 2
-
-
 )
